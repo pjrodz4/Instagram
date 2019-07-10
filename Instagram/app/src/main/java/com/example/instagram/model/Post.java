165f8bc8f99243ef1a6ad5a8530be48e476f548b
pjrodz4 <pjrodz4@gmail.com>
2019-07-10 15:54:11 -0700

    Implemented login, signup, image capturing, and bottom navigation view.

--- Instagram/app/src/main/java/com/example/instagram/model/Post.java
+++ Instagram/app/src/main/java/com/example/instagram/model/Post.java
@@ -32,7 +32,7 @@ public class Post extends ParseObject {
         return getParseUser(KEY_USER);
     }
 
-    public void putUser(ParseUser user) {
+    public void setUser(ParseUser user) {
         put(KEY_USER, user);
     }
 
